bde126de5312c5b8a5314bcca422a16894bd3a8e
Eliecer Arteaga <eliecerj@gmail.com>
2019-11-28 08:09:16 -0300

    user middleware file

---
+++ src/middleware/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+
+const auth = async (req, res, next) => {
+    try {
+        //console.log('hola')
+        const token = req.header('Authorization').replace('Bearer ', '')
+        const decoded = jwt.verify(token, 'thisismynewcourse')
+        //console.log('tokennn', token)
+        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
+
+        if(!user) {
+            throw new Error()
+        }
+
+        req.user = user
+        next()
+    } catch (e) {
+        res.status(401).send( {error: 'Please authenticate.'})
+    }
+}
+
+module.exports = auth
