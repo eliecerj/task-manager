d4c8f267ec6ecc812844449667b88f5619cbf174
Eliecer Arteaga <eliecerj@gmail.com>
2019-12-04 22:47:48 -0300

    Auto-Cropping and Image Formatting

--- src/index.js
+++ src/index.js
@@ -8,30 +8,6 @@ const TaskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-const multer = require('multer')
-const upload = multer({
-    dest: 'images',
-    limits: {
-        fileSize: 1000000
-    },
-    fileFilter(req, file, cb) {
-        if(!file.originalname.match(/\.(doc|docx)$/)) {
-            return cb(new Error('Please upload a Word documentbbb'))
-        }
-
-        cb(undefined, true)
-        // cb(new Error('file must be a PDF'))
-        // cb(undefined, true)
-        // cb(undefined, false)
-    }
-})
-
-app.post('/upload', upload.single('upload'), (req, res) => {
-    res.send()
-}, (error, req, res, next) => {
-    res.status(400).send({ error: error.message})
-})
-
 app.use(express.json())
 app.use(UserRouter)
 app.use(TaskRouter)
